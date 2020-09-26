7117c1e2b9fc06aba27276cee9dd57010108ff65
Adam Gausmann <agausmann@fastmail.com>
2020-09-26 05:16:12 -0500

    Add fundamental traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,21 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+//! A declarative I/O serialization library.
+
+#![warn(missing_docs)]
+
+use std::io;
+
+/// A type that can be serialized into a byte stream.
+pub trait Serialize<Ctx> {
+    /// Serializes `&self` to the given writer.
+    fn serialize<W>(&self, ctx: Ctx, writer: &mut W) -> Result<(), io::Error>
+    where
+        W: io::Write;
+}
+
+/// A type that can be deserialized from a byte stream.
+pub trait Deserialize<Ctx>: Sized {
+    /// Deserializes a value from the given reader.
+    fn deserialize<R>(ctx: Ctx, reader: &mut R) -> Result<Self, io::Error>
+    where
+        R: io::Read;
 }
