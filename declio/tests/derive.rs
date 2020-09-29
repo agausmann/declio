fe242552768fd9078238b5718323808bca3cf54b
Adam Gausmann <agausmann@fastmail.com>
2020-09-29 14:43:46 -0500

    Update derive test

--- declio/tests/derive.rs
+++ declio/tests/derive.rs
@@ -1,42 +1,43 @@
-use declio::Serialize;
+use declio::Encode;
 
-fn test_serialize<T>(input: T, expected: &[u8])
+fn test_encode<T>(input: T, expected: &[u8])
 where
-    T: Serialize,
+    T: Encode,
 {
     let mut output = Vec::new();
-    input.serialize((), &mut output).unwrap();
+    input.encode((), &mut output).unwrap();
     assert_eq!(output, expected);
 }
 
 #[test]
-fn test_unit_struct_serialize() {
-    #[derive(Serialize)]
+fn test_unit_struct_encode() {
+    #[derive(Encode)]
+    #[declio(bound = "UnitStruct: Encode")]
     struct UnitStruct;
 
-    test_serialize(UnitStruct, &[]);
+    test_encode(UnitStruct, &[]);
 }
 
 #[test]
-fn test_tuple_struct_serialize() {
-    #[derive(Serialize)]
+fn test_tuple_struct_encode() {
+    #[derive(Encode)]
     struct TupleStruct(u8, u32);
 
-    test_serialize(
+    test_encode(
         TupleStruct(0xab, 0xdeadbeef),
         &[0xab, 0xef, 0xbe, 0xad, 0xde],
     );
 }
 
 #[test]
-fn test_struct_serialize() {
-    #[derive(Serialize)]
+fn test_struct_encode() {
+    #[derive(Encode)]
     struct Struct {
         x: u8,
         y: u32,
     }
 
-    test_serialize(
+    test_encode(
         Struct {
             x: 0xab,
             y: 0xdeadbeef,
